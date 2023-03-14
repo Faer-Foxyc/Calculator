e7dee38afe7b20904e9d882bf44f54de46ccaeec
Anton Fox <42889325+AntonFoxy@users.noreply.github.com>
2023-03-14 08:52:02 +0300

    Ветка 14.032023 8:51

--- Data/DataClass.cs
+++ Data/DataClass.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Калькулятор.Data
+﻿namespace Калькулятор.Data
 {
     internal class DataClass
     {
-        public int NumbOne { get; set; } = 1;
-        public int NumbTwo { get; set; } = 2;
-        public int NumbThree { get; set; } = 3;
+        public string Plus { get; set; } = "+";
+        public string Minus { get; set; } = "-";
+        public string Division { get; set; } = "/";
+        public string Multyplication { get; set; } = "*";
+        public string Equally { get; set; } = "=";
+        public string Point { get; set; } = ".";
+        public string Procent { get; set; } = "%";
+        public string Root { get; set; } = "√";
+
+        public int GetNumber(int Numb)
+        {
+            return Numb;
+        }
+        
+        /// <summary>
+        /// Метод который удаляет 1 символ из строки. Так же 
+        /// данный метод ловит исключение, что бы не было ошибки
+        /// при полном удалении всех символов в строке
+        /// </summary>
+        /// <param name="Text"></param>
+        /// Метод принимающий параметр в видет строки
+        /// <returns></returns>
+        public string _DeleteSumbol(string Text)
+        {
+            int _IndexSumbol = Text.Length -1;
+            try
+            {
+                Text = Text.Remove(_IndexSumbol);
+            }
+            catch (System.Exception) { }
+
+            return Text;
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,82 +17,102 @@ namespace Калькулятор
 
         private void btn_1_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += _dataClass.NumbOne;
+            tb_Text.Text += _dataClass.GetNumber(1);
         }
 
         private void btn_2_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += _dataClass.NumbTwo;
+            tb_Text.Text += _dataClass.GetNumber(2);
         }
 
         private void btn_3_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += _dataClass.NumbThree;
+            tb_Text.Text += _dataClass.GetNumber(3);
         }
 
         private void btn_4_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "4";
+            tb_Text.Text += _dataClass.GetNumber(4);
         }
 
         private void btn_5_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "5";
+            tb_Text.Text += _dataClass.GetNumber(5);
         }
 
         private void btn_6_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "6";
+            tb_Text.Text += _dataClass.GetNumber(6);
         }
 
         private void btn_7_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "7";
+            tb_Text.Text += _dataClass.GetNumber(7);
         }
 
         private void btn_8_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "8";
+            tb_Text.Text += _dataClass.GetNumber(8);
         }
 
         private void btn_9_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "9";
+            tb_Text.Text += _dataClass.GetNumber(9);
         }
 
         private void btn_0_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "0";
+            tb_Text.Text += _dataClass.GetNumber(0);
         }
 
         private void btn_Division_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "/";
+            tb_Text.Text += _dataClass.Division;
         }
 
         private void btn_Plus_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "+";
+            tb_Text.Text += _dataClass.Plus;
         }
 
         private void btn_Multiplication_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "*";
+            tb_Text.Text += _dataClass.Multyplication;
         }
 
         private void Equally_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "=";
+            tb_Text.Text += _dataClass.Equally;
         }
 
         private void Point_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += ".";
+            tb_Text.Text += _dataClass.Point;
         }
 
         private void Minus_Click(object sender, RoutedEventArgs e)
         {
-            tb_Text.Text += "-";
+            tb_Text.Text += _dataClass.Minus;
+        }
+
+        private void btn_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            tb_Text.Clear();
+        }
+
+        private void btn_Procent_Click(object sender, RoutedEventArgs e)
+        {
+            tb_Text.Text = _dataClass.Procent;
+        }
+
+        private void btn_Root_Click(object sender, RoutedEventArgs e)
+        {
+            tb_Text.Text = _dataClass.Root;
+        }
+
+        private void btn_BackSpace_Click(object sender, RoutedEventArgs e)
+        {
+            tb_Text.Text = _dataClass._DeleteSumbol(tb_Text.Text);
         }
     }
 }
